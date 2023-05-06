b8f7b43874708c05148556cd52b8aad890bb9884
Moisés Conte <mtfc.dk@hotmail.com>
2023-05-06 18:28:55 -0300

    master

--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -1,8 +1,8 @@
-import { UniqueEntityId } from '../../core/entities/unique-entity-id'
-import { Answer } from '../entities/answer'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Answer } from '../../enterprise/entities/answer'
 import { AnswersRepository } from '../repositories/answers-repository'
 
-interface AnswerQuestionRequest {
+interface AnswerQuestionUseCaseRequest {
   instructorId: string
   questionId: string
   content: string
@@ -11,7 +11,11 @@ interface AnswerQuestionRequest {
 export class AnswerQuestionUseCase {
   constructor(private answersRepository: AnswersRepository) {}
 
-  async execute({ instructorId, questionId, content }: AnswerQuestionRequest) {
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest) {
     const answer = Answer.create({
       content,
       authorId: new UniqueEntityId(instructorId),
