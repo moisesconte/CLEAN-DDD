acaf26853a12d92458f93fecef50507bd9095648
Moisés Conte <mtfc.dk@hotmail.com>
2023-05-28 10:37:56 -0300

    master

--- src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -2,14 +2,13 @@ import { Either, left, right } from '@/core/either'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
-import { NotAllowedError } from './errors/not-allowed-error'
 
 interface GetQuestionBySlugUseCaseRequest {
   slug: string
 }
 
 type GetQuestionBySlugUseCaseResponse = Either<
-  ResourceNotFoundError | NotAllowedError,
+  ResourceNotFoundError,
   {
     question: Question
   }
